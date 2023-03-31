d3375ece1b7773a4ec4b0f46a630803a3fe36337
Chalini11 <chalini.pasupathy@m2pfintech.com>
2023-03-31 12:28:39 +0530

    Intial commit to create folder

---
+++ src/main/java/com/m2pfintech/CoinProbability.java
@@ -0,0 +1,6 @@
+package com.m2pfintech;
+
+public class CoinProbability
+{
+
+}
---
+++ src/test/java/com/m2pfintech/CoinProbabilityTest.java
@@ -0,0 +1,6 @@
+package com.m2pfintech;
+
+public class CoinProbabilityTest
+{
+    CoinProbability coinObject = new CoinProbability();
+}
